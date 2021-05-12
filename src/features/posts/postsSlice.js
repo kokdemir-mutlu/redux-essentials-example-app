99766cbb76774c5fa5292a7ecae75333b7e41dc0
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-05-12 19:45:39 +0300

    creation of postsSlice and update the store

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+	{
+		id : '1',
+		title : 'First Post',
+		content : 'Hello!',
+	},
+	{
+		id : '2',
+		title : 'Second Post',
+		content : 'More text',
+	}
+];
+
+const postsSlice = createSlice({
+	name : 'posts',
+	initialState,
+	reducers : {}
+});
+
+
+export default postsSlice.reducer;
+
